87aecac063272cf2cd70f503ccecb1b15530f1ab
RichardMau5 <richarddirven@outlook.com>
2017-12-07 16:36:20 +0100

    Added bat script to launch multiple applications and first code

--- NetwProg/NetwProg/Program.cs
+++ NetwProg/NetwProg/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NetwProg
+
+class NetwProg
 {
-    class Program
+    int myPortNr;
+    List<int> neighBourPortNrs;
+
+    public NetwProg(string[] args)
     {
-        static void Main(string[] args)
+        myPortNr = int.Parse(args[0]);
+        Console.Title = "NetChange " + myPortNr;
+        for(int i = 1; i < args.Length; i++)
         {
+
         }
+        Console.ReadLine();
+    }
+
+    static void Main(string[] args)
+    {
+        if (args.Length != 0)
+        {
+            NetwProg p = new NetwProg(args);
+            //p.Run();
+        }
+        else { Console.WriteLine("Please provide at least a socket nr."); }
     }
 }
