ee09d5200d26ddf58fe53a54fd192dd7d46e1132
RichardMau5 <richarddirven@outlook.com>
2017-12-09 18:57:00 +0100

    Added rudimentary inter process communication

---
+++ NetwProg/NetwProg/MultiClientServer/Server.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+class Server
+{
+    public Server(int port)
+    {
+        // Luister op de opgegeven poort naar verbindingen
+        TcpListener server = new TcpListener(IPAddress.Any, port);
+        server.Start();
+
+        // Start een aparte thread op die verbindingen aanneemt
+        new Thread(() => AcceptLoop(server)).Start();
+    }
+
+    private void AcceptLoop(TcpListener handle)
+    {
+        while (true)
+        {
+            TcpClient client = handle.AcceptTcpClient();
+            StreamReader clientIn = new StreamReader(client.GetStream());
+            StreamWriter clientOut = new StreamWriter(client.GetStream());
+            clientOut.AutoFlush = true;
+
+            // De server weet niet wat de poort is van de client die verbinding maakt, de client geeft dus als onderdeel van het protocol als eerst een bericht met zijn poort
+            int zijnPoort = int.Parse(clientIn.ReadLine().Split()[1]);
+
+            Console.WriteLine("Client maakt verbinding: " + zijnPoort);
+
+            // Zet de nieuwe verbinding in de verbindingslijst
+            if(NetwProg.neighs[zijnPoort] == null)
+                NetwProg.neighs[zijnPoort] = new Connection(clientIn, clientOut);
+        }
+    }
+}
