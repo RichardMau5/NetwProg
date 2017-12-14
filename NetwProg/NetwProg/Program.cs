7d905dc3681773ab0cb664baf168bd12028543c8
RichardMau5 <richarddirven@outlook.com>
2017-12-14 11:21:14 +0100

    Added new files

--- NetwProg/NetwProg/Program.cs
+++ NetwProg/NetwProg/Program.cs
@@ -7,17 +7,13 @@ using System.Threading.Tasks;
 
 class NetwProg
 {
-    int myPortNr;
-    List<int> neighBourPortNrs;
+    public static int myPortNr;
+    public static Dictionary<int, Connection> neighs = new Dictionary<int, Connection>();
+    Dictionary<int, int> routingNeighTable = new Dictionary<int, int>();
+
 
     public NetwProg(string[] args)
     {
-        myPortNr = int.Parse(args[0]);
-        Console.Title = "NetChange " + myPortNr;
-        for(int i = 1; i < args.Length; i++)
-        {
-
-        }
         Console.ReadLine();
     }
 
@@ -25,9 +21,43 @@ class NetwProg
     {
         if (args.Length != 0)
         {
-            NetwProg p = new NetwProg(args);
-            //p.Run();
+            Init(args);
+            Run();
         }
         else { Console.WriteLine("Please provide at least a socket nr."); }
+    }
+
+    private static void Init(string[] args)
+    {
+        myPortNr = int.Parse(args[0]);
+        Console.Title = "NetChange " + myPortNr;
+        new Server(myPortNr);
+        for (int i = 1; i < args.Length; i++)
+        {
+            int port = int.Parse(args[i]);
+            neighs.Add(port, null);
+        }
+    }
+
+    private static void Run()
+    {
+        while (true)
+        {
+            string[] input = Console.ReadLine().Split(new char[] { ' ' }, 3);
+            switch (input[0])
+            {
+                case "B":
+                    int portMsg = int.Parse(input[1]);
+                    if (!neighs.ContainsKey(portMsg))
+                    {
+                        Console.WriteLine("Error: Unknown port number");
+                        break;
+                    }
+                    if (neighs[portMsg] == null)
+                        neighs[portMsg] = new Connection(portMsg);
+                    neighs[portMsg].Write.WriteLine(input[2]);
+                    break;
+            }
+        }
     }
 }
